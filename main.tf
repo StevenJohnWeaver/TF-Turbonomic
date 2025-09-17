162b8112cd0ef174e38fc1c9d298ee70f4d75bd0
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-09-17 10:31:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,11 +40,20 @@ variable "turbo_hostname" {
   sensitive   = false
 }
 
+data "turbonomic_cloud_entity_recommendation" "example" { 
+  entity_name  = "exampleVirtualMachine" // Name of the cloud entity (e.g., a virtual machine) to get recommendations for 
+  entity_type  = "VirtualMachine" // Type of the cloud entity (e.g., VirtualMachine, Database, etc.) 
+  default_size = "t3.nano" // Default instance size used if no recommendation is available. 
+}
+
 # Create an AWS EC2 instance
-resource "aws_instance" "web_server" {
+resource "aws_instance" "terraform-demo-ec2" { 
   ami           = "ami-0de716d6197524dd9" # This is a publicly available Amazon Linux 2 AMI
-  instance_type = "t2.small"
-  tags = {
-    Name = "HelloWorldServer"
-  }
+  instance_type = data.turbonomic_cloud_entity_recommendation.example.new_instance_type // Uses the recommended instance type from the Turbonomic data source. 
+  tags = merge( 
+    { 
+      Name = "exampleVirtualMachine" // Sets the Name tag for easy identification 
+    }, 
+    provider::turbonomic::get_tag() //tag the resource as optimized by Turbonomic Provider for Terraform 
+  ) 
 }
